e87729a13f21d9350acea3013ee1ad708bcbf63e
Estevão Simões <estevao.simoes@live.com>
2023-07-25 22:43:55 -0300

    responsive fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,8 +68,8 @@
                     </div>
                 </div>
             </nav>
-            <div class="container px-6 pt-20 pb-24 mx-auto md:text-center md:px-4">
-                <h1 class="font-extrabold leading-snug text-white text-5xl md:text-6xl xl:text-7xl">
+            <div class="container px-6 pt-11 md:pt-20 pb-24 mx-auto md:text-center md:px-4">
+                <h1 class="font-extrabold leading-snug text-white text-4xl md:text-6xl xl:text-7xl">
                     <span class="block">
                         Plataforma de oportunidades
                     </span>
@@ -77,18 +77,22 @@
                         <span class="text-vermilion">laravel</span> no Brasil
                     </span>
                 </h1>
-                <p
-                    class="mx-auto mt-6 text-sm text-left text-gray-200 md:text-center md:mt-12 sm:text-base md:max-w-xl md:text-lg xl:text-xl">
+                <p class="mx-auto mt-10 md:mt-24 text-sm text-left text-gray-200 md:text-center sm:text-base md:max-w-xl md:text-lg xl:text-xl">
                     Inscreva-se para receber notificações de novas vagas de empresas que utilizam Laravel no Brasil.
                 </p>
-                <div
-                    class="relative flex items-center mx-auto mt-12 overflow-hidden text-left border border-gray-700 rounded-md md:max-w-md md:text-center">
-                    <input type="text" name="email" placeholder="E-mail"
-                        class="w-full h-12 px-6 py-2 font-medium text-gray-800 focus:outline-none">
-                    <span class="relative top-0 right-0 block">
-                        <button type="button"
-                            class="backdrop-opacity-10 backdrop-invert inline-flex items-center w-40 h-12 px-8 text-base font-bold leading-3 text-white transition duration-150 ease-in-out bg-gray-800 border border-transparent hover:bg-gray-700 focus:outline-none active:bg-gray-700"
-                            data-primary="gray-600">
+                <div class="flex items-center flex-col md:flex-row mx-auto mt-8 overflow-hidden text-left border border-gray-700 rounded-md md:max-w-md md:text-center">
+                    <input 
+                        type="text"
+                        name="email"
+                        placeholder="E-mail"
+                        class="w-full h-12 px-3 md:px-6 py-2 text-sm md:text-base font-medium text-gray-800 focus:outline-none"
+                    >
+                    <span class="md:relative md:top-0 md:right-0 block w-full md:w-auto">
+                        <button 
+                            type="button"
+                            class="inline-flex items-center justify-center w-full h-12 px-2 text-xs md:w-32 md:px-4 md:text-base font-bold leading-3 text-white transition duration-150 ease-in-out bg-gray-800 border border-transparent hover:bg-gray-700 focus:outline-none active:bg-gray-700"
+                            data-primary="gray-600"
+                        >
                             Inscrever-se
                         </button>
                     </span>
