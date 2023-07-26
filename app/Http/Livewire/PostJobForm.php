07973cfca051cf9aa64c5d0e5e5ef59f12f3a919
Estevão Simões <estevao.simoes@live.com>
2023-07-26 00:59:46 -0300

    chore: form wip

--- app/Http/Livewire/PostJobForm.php
+++ app/Http/Livewire/PostJobForm.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Jobs\Listing;
 use Livewire\Component;
 use Filament\Forms;
+use Filament\Notifications\Notification;
+use Illuminate\Validation\ValidationException;
 
 class PostJobForm extends Component implements Forms\Contracts\HasForms
 {
-    use Forms\Concerns\InteractsWithForms; 
+    use Forms\Concerns\InteractsWithForms;
 
     public $title;
     public $content;
- 
+
     public function mount(): void
     {
         $this->form->fill([
@@ -19,16 +22,56 @@ class PostJobForm extends Component implements Forms\Contracts\HasForms
             'content' => '',
         ]);
     }
- 
-    protected function getFormSchema(): array 
+
+    protected function getFormSchema(): array
     {
         return [
-            Forms\Components\TextInput::make('title')->required(),
-            Forms\Components\MarkdownEditor::make('content'),
+            Forms\Components\TextInput::make('title')
+                ->label('Título do cargo')
+                ->required(),
+            Forms\Components\TextInput::make('locale')
+                ->label('Local')
+                ->helperText('Ex.: São Paulo, SP, Remoto, Híbrido')
+                ->required(),
+            Forms\Components\Select::make('contract_type')
+                ->label('Tipo de contrato')
+                ->options([
+                    'FULL_TIME' => 'Tempo integral',
+                    'PART_TIME' => 'Meio período',
+                    'CONTRACTOR' => 'Consultor',
+                    'TEMPORARY' => 'Temporário',
+                    'INTERN' => 'Estagiário',
+                    'VOLUNTEER' => 'Voluntário',
+                    'PER_DIEM' => 'Por dia',
+                    'OTHER' => 'Outro'
+                ])
+                ->required(),
+            Forms\Components\TextInput::make('url')
+                ->label('Link da Vaga')
+                ->activeUrl()
+                ->required(),
+            Forms\Components\TextInput::make('salary')
+                ->label('Faixa salarial (opcional)')
+                ->helperText('Ex.: R$ 2.000,00 - R$ 3.000,00'),
+            Forms\Components\Select::make('tags')
+                ->label('Tags')
+                ->multiple()
+                ->helperText('Selecione até 4 tags')
+                ->options(Listing::getTags()->toArray())
+                ->optionsLimit(4)
+                ->required(),
             // ...
         ];
-    } 
- 
+    }
+
+    protected function onValidationError(ValidationException $exception): void
+    {
+        Notification::make()
+            ->title($exception->getMessage())
+            ->danger()
+            ->send();
+    }
+
     public function submit(): void
     {
         // ...
@@ -36,7 +79,6 @@ class PostJobForm extends Component implements Forms\Contracts\HasForms
 
     public function render()
     {
-        return view('livewire.post-job-form'); 
+        return view('livewire.post-job-form');
     }
-
 }
