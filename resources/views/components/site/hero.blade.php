964f906decb7101ba9f2861c9b6eccfeb88d85c4
Estevão Simões <estevao.simoes@live.com>
2023-07-28 00:13:10 -0300

    SEO

--- resources/views/components/site/hero.blade.php
+++ resources/views/components/site/hero.blade.php
@@ -1,7 +1,7 @@
 <div class="container px-6 pt-11 md:pt-20 pb-24 mx-auto md:text-center md:px-4">
     <h1 class="font-extrabold leading-snug text-white text-4xl md:text-6xl xl:text-7xl">
         <span class="block">
-            Plataforma de oportunidades
+            Painel de vagas
         </span>
         <span class="relative inline-block text-white md:mt-4">
             <span class="text-vermilion">laravel</span> no Brasil
