318ec67add34f7e6e06dbe737758c56e50d65c21
Estevão Simões <estevao.simoes@live.com>
2023-07-28 15:56:52 -0300

    product id update

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('auth')->group(function () {
     })->name('post-job.view');
 
     Route::get('/charge-checkout/{listing}', function (Listing $listing, Request $request) {
-        return $request->user()->checkout(['price_1NYutUKCVKM4D3MpXAAJLSxy' => 1], [
+        return $request->user()->checkout(['price_1NYvphKCVKM4D3Mp08Bhuxcg' => 1], [
             'success_url' => route('checkout-success').'?session_id={CHECKOUT_SESSION_ID}&listing_id=' . $listing->id,
             'cancel_url' => route('dashboard'),
         ]);
